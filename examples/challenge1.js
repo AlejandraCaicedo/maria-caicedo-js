7de82d45fb802b360d3dbf710039d214497abb66
AlejandraCaicedo <caicedomaria905@gmail.com>
2021-11-23 15:14:54 -0500

    Solving diagonal difference problem

--- examples/challenge1.js
+++ examples/challenge1.js
@@ -46,3 +46,15 @@ function simpleArraySum(ar) {
   let sum = ar.reduce((n1, n2) => n1 + n2, 0)
   return sum
 }
+
+// Diagonal difference
+function diagonalDifference(arr) {
+  // Write your code here
+  let primaryDiagonal = 0
+  let secondaryDiagonal = 0
+  for (let i = 0; i < arr.length; i++) {
+    primaryDiagonal += arr[i][i]
+    secondaryDiagonal += arr[i][arr.length - i - 1]
+  }
+  return Math.abs(primaryDiagonal - secondaryDiagonal)
+}
