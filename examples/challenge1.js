c30aaad8d604ce23b46cc52a7d5947dc3425528c
AlejandraCaicedo <caicedomaria905@gmail.com>
2021-11-23 11:18:52 -0500

    Adding strong password  problem

--- examples/challenge1.js
+++ examples/challenge1.js
@@ -1,11 +1,42 @@
 // Camelcase problem
 function camelcase(s) {
   let count = 1
-  for(let i=0; i<s.length; i++){
+  for (let i = 0; i < s.length; i++) {
     let char = s.charAt(i)
-    if( char == char.toUpperCase()){
-        count +=1
+    if (char == char.toUpperCase()) {
+      count += 1
     }
   }
   return count
+}
+
+// Strong password problem
+function minimumNumber(n, password) {
+  // Return the minimum number of characters to make the password strong
+  let minumun = 0
+
+  if (n < 6) {
+    minumun = 6 - n
+  } else {
+    let specialCharacter = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+']
+    let check = [false, false, false, false]
+    for (let i = 0; i < n; i++) {
+      let char = password.charAt(i)
+      if (specialCharacter.includes(char) && check[0] == false) {
+        check[0] = true
+      } else if (!specialCharacter.includes(char) && char === char.toLowerCase() && check[1] == false) {
+        check[1] = true
+      } else if (+char.isInteger && !specialCharacter.includes(char) && check[2] == false) {
+        check[2] = true
+      } else if (!specialCharacter.includes(char) && char === char.toUpperCase() && check[3] == false) {
+        check[3] = true
+      }
+    }
+    if (check.length == check.filter(check => check == true).length) {
+      minumun = 0
+    } else {
+      minumun = check.filter(check => check == false).length
+    }
+  }
+  return minumun
 }
