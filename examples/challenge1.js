122ea330627671950657407b00da48b9720179d3
AlejandraCaicedo <caicedomaria905@gmail.com>
2021-11-25 12:04:02 -0500

    Final solution to strong password problem

--- examples/challenge1.js
+++ examples/challenge1.js
@@ -12,31 +12,60 @@ function camelcase(s) {
 
 // Strong password problem
 function minimumNumber(n, password) {
-  // Return the minimum number of characters to make the password strong
-  let minumun = 0
+  let check = [false, false, false, false]
+  let minumun = 0;
 
-  if (n < 6) {
-    minumun = 6 - n
+  let specialCharacter = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+']
+
+  for (let i = 0; i < n; i++) {
+    if (password.charCodeAt(i) >= 65 && password.charCodeAt(i) <= 90 && check[0] == false) check[0] = true
+    if (password.charCodeAt(i) >= 97 && password.charCodeAt(i) <= 122 && check[1] == false) check[1] = true
+    if (specialCharacter.includes(password.charAt(i)) && check[2] == false) check[2] = true
+    if (password.charCodeAt(i) >= 48 && password.charCodeAt(i) <= 57 && check[3] == false) check[3] = true
+  }
+  if (!check[0]) minumun++
+  if (!check[1]) minumun++
+  if (!check[2]) minumun++
+  if (!check[3]) minumun++
+
+  if ((n + minumun) < 6) {
+    return ((6 - (n + minumun)) + minumun);
+  }
+  else {
+    return minumun;
+  }
+}
+
+// Simple array sum
+function simpleArraySum(ar) {
+  let sum = ar.reduce((n1, n2) => n1 + n2, 0)
+  return sum
+}
+
+// Diagonal difference
+function diagonalDifference(arr) {
+  // Write your code here
+  let primaryDiagonal = 0
+  let secondaryDiagonal = 0
+  for (let i = 0; i < arr.length; i++) {
+    primaryDiagonal += arr[i][i]
+    secondaryDiagonal += arr[i][arr.length - i - 1]
+  }
+  return Math.abs(primaryDiagonal - secondaryDiagonal)
+}
+
+// The power sum
+function powerSum(X, N, next, sum) {
+  if (sum == X) {
+    return 1
   } else {
-    let specialCharacter = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+']
-    let check = [false, false, false, false]
-    for (let i = 0; i < n; i++) {
-      let char = password.charAt(i)
-      if (specialCharacter.includes(char) && check[0] == false) {
-        check[0] = true
-      } else if (!specialCharacter.includes(char) && char === char.toLowerCase() && check[1] == false) {
-        check[1] = true
-      } else if (+char.isInteger && !specialCharacter.includes(char) && check[2] == false) {
-        check[2] = true
-      } else if (!specialCharacter.includes(char) && char === char.toUpperCase() && check[3] == false) {
-        check[3] = true
+    let count = 0
+
+    if (sum < X) {
+      for (let i = next; i <= (X / N); i++) {
+        count += powerSum(X, N, i + 1, sum + Math.pow(i, N))
       }
     }
-    if (check.length == check.filter(check => check == true).length) {
-      minumun = 0
-    } else {
-      minumun = check.filter(check => check == false).length
-    }
+    return count
   }
-  return minumun
 }
