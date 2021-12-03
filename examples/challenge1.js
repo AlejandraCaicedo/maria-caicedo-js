820d87502862f18643ada6bd0a193da0db659ab9
AlejandraCaicedo <caicedomaria905@gmail.com>
2021-12-03 11:52:19 -0500

    Adding documentation to all functions
    Update challenge1.js
    
    Update challenge1.js

--- examples/challenge1.js
+++ examples/challenge1.js
@@ -1,4 +1,8 @@
-// Camelcase problem
+/**
+ * Function to find the number of words in s following the camelCase style
+ * @param {string} s string to analyze
+ * @returns {number} the number of words in s
+ */
 function camelcase(s) {
   let count = 1
   for (let i = 0; i < s.length; i++) {
@@ -10,9 +14,13 @@ function camelcase(s) {
   return count
 }
 
-// Strong password problem
+/**
+ * Function to find the minimum number of characters to make the password strong
+ * @param {number} n the length of the password
+ * @param {string} password the password to test
+ * @returns {number} the minimun number of characters to add
+ */
 function minimumNumber(n, password) {
-  // Return the minimum number of characters to make the password strong
   let minumun = 0
 
   if (n < 6) {
@@ -41,17 +49,26 @@ function minimumNumber(n, password) {
   return minumun
 }
 
-// Simple array sum
+/**
+ * Function that given an array of integers, finds the sum of its elements
+ * @param {array} ar an array of integers 
+ * @returns {number} the sum of the array's elements as a single integer
+ */
 function simpleArraySum(ar) {
   let sum = ar.reduce((n1, n2) => n1 + n2, 0)
   return sum
 }
 
-// Diagonal difference
+/**
+ * Function that given a square matrix,
+ * Calculates the absolute difference between the sums of its diagonals
+ * @param {array} arr an array of integers
+ * @returns {number} the absolute diagonal difference
+ */
 function diagonalDifference(arr) {
-  // Write your code here
   let primaryDiagonal = 0
   let secondaryDiagonal = 0
+
   for (let i = 0; i < arr.length; i++) {
     primaryDiagonal += arr[i][i]
     secondaryDiagonal += arr[i][arr.length - i - 1]
@@ -59,7 +76,15 @@ function diagonalDifference(arr) {
   return Math.abs(primaryDiagonal - secondaryDiagonal)
 }
 
-// The power sum
+/**
+ * Recursive function that calculates the number of possible combinations that a 
+ * Number can be expressed as a sum of powers
+ * @param {number} X the integer to be found by power combinations 
+ * @param {number} N the integer power to raise numbers to
+ * @param {number} next initial value 1, the integer where nested iteration starts, the next value to raise
+ * @param {number} sum initial value 0, the sum of powers found in a complete iteration
+ * @returns {number} the number of possible combinations calculated
+ */
 function powerSum(X, N, next, sum) {
   if (sum == X) {
     return 1
